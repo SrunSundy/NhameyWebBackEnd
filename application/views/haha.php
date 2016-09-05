ac09b6df9f9e43b8c38aa1aca9574947bd55a7d5
sundy <jame001sundy@gmail.com>
2016-09-05 15:59:42 +0700

    Nhamery project
    Be passionate

---
+++ application/views/haha.php
@@ -0,0 +1,17 @@
+<?php
+	$yummy = json_decode($json);
+	echo $yummy->name;
+?>
+<!DOCTYPE html> 
+<html lang = "en"> 
+
+   <head> 
+      <meta charset = "utf-8"> 
+      <title>CodeIgniter View Example</title> 
+   </head>
+	
+   <body> 
+      CodeIgniter View Example ssssssssssssssssssssssssssssss
+   </body>
+	
+</html>
